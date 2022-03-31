714386d8c82cf70bc10f5657afce6e01301cea37
Megan Campbell <megancampbell201@gmail.com>
2022-03-31 17:26:28 -0400

    created favorite route and model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,16 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes.js');
-const recipeRoutes = require('./recipe-routes');
-const commentRoutes = require('./comment-routes');
-const categoryRoutes = require('./category-routes');
+//const userRoutes = require('./user-routes.js');
+//const recipeRoutes = require('./recipe-routes');
+//const commentRoutes = require('./comment-routes');
+const categoryRoutes = require("./category-routes");
+const favoriteRoutes = require("./favorite-routes");
 
-
-router.use('/users', userRoutes);
-router.use('/recipes', recipeRoutes);
-router.use('/comments', commentRoutes);
-router.use('/categories', categoryRoutes);
-
-
+//router.use('/users', userRoutes);
+//router.use('/recipes', recipeRoutes);
+//router.use('/comments', commentRoutes);
+router.use("/categories", categoryRoutes);
+router.use("./favorites", favoriteRoutes);
 
 module.exports = router;
