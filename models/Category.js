94b084148ac3af15e3971112d9e9ddc831508838
Megan Campbell <megancampbell201@gmail.com>
2022-03-31 09:26:54 -0400

    added category routes and model

--- models/Category.js
+++ models/Category.js
@@ -1,21 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Category extends Model{}
+class Category extends Model {}
 
-Category.init({
+Category.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      category_name: {
-          type: DataTypes.STRING,
-          allowNull: false
-      }
-})
-
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
 
 module.exports = Category;
