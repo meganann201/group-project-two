437e81c10b84e4797dc9a7a0bd61a96835e3ee46
vieiragcv <vieiragcv@gmail.com>
2022-04-04 21:46:04 -0400

    finalized user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,9 +3,6 @@ const router = express.Router();
 
 const { User } = require('../../models');
 
-/* const inputCheck = require('../../utils/inputCheck'); */
-
-
 /*---------------------------------------------------------------
 -                         GET ALL USERS PROFILES
 ---------------------------------------------------------------*/
@@ -23,46 +20,60 @@ router.get('/', async (req, res) => {
 -                       GET 1 USER PROFILE
 ---------------------------------------------------------------*/
 
-/* router.get('/users/:id', (req, res) => {
-  const sql = `SELECT * FROM users WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
+router.get('/:id', (req, res) => {
+    User.findOne({
+      where:{
+        id: req.params.id
+      },
+    })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+      
+/*---------------------------------------------------------------
+-                      UDPATE USER PROFILE
+---------------------------------------------------------------*/
+
+router.put("/:id", (req, res) => {
+  User.update(
+    {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      user_name: req.body.user_name,
+      secret_key: req.body.secret_key,
+    },
+    {
+    where: {
+      id: req.params.id
     }
-    res.json({
-      message: 'success',
-      data: row
+   })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  });
-}); */
+});  
 
 /*---------------------------------------------------------------
 -                       DELETE 1 USER PROFILE
 ---------------------------------------------------------------*/
 
-/* router.delete('/users/:id', (req, res) => {
-  const sql = `DELETE FROM users WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      // checks if anything was deleted
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'User not found'
-      });
-    } else {
-      res.json({
-        message: 'user deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
     }
+  })
+  .then((dbUserData) => res.json(dbUserData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
   });
-}); */
-
+})
 
 /*---------------------------------------------------------------
 -                      CREATE USER PROFILE
@@ -85,6 +96,5 @@ router.post('/adduser', async ({ body }, res) => {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
